5f9139b914b05a1e279e050b4dea97b537684db8
Martin Lopez <melopez831@misena.edu.co>
2020-03-09 08:28:37 -0500

    Prueba de commit 2

---
+++ prueba/prueba/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("esto es una prueba de git");
+            Console.ReadKey();
+        }
+    }
+}
